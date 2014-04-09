7108664c25596416788b41827523b78d7899d22c
kw4ja <kw4ja@naver.com>
2014-04-09 11:44:32 +0900

    140214

---
+++ 140214/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+
+//
+/*
+
+영타장문 시험 160타이상
+진수변환 시험 5문제
+타임어텍 3분
+cout << "이름\t국어\t영어\t수학" << endl;
+cout << " 홍\t100\t100\t100" << endl;
+cout << " 길\t 98\t 45\t 65" << endl;
+cout << " 동\t 10\t 20\t  0" << endl;
+*/
+void main(void)
+{
+	cout << "*" << endl;
+	cout << "***" << endl;
+	cout << "*****" << endl;
+	cout << "*******" << endl;
+	cout << "*********" << endl << endl;
+
+	cout << "        *" << endl;
+
+	cout << "      ***" << endl;
+	cout << "    *****" << endl;
+	cout << "  *******" << endl;
+	cout << "*********" << endl << endl;
+
+	cout << "    *" << endl;
+	cout << "   ***" << endl;
+	cout << "  *****" << endl;
+	cout << " *******" << endl;
+	cout << "*********" << endl << endl;
+
+
+	cout << "10+10 = " << 10+10 << endl;
+	cout << "10-10 = " << 10-10 << endl;
+	cout << "10/10 = " << 10/10 << endl;
+	cout << "10*10 = " << 10*10 << endl;
+	cout << " 띄 어 쓰 기 " << endl;
+	cout << "<< 를" << "이용" << endl;
+	cout << endl;
+	cout << "문자 10  " << "숫자 " << 10 << endl;
+	cout << endl << endl << endl;
+
+
+	cout << "이름\t국어\t영어\t수학" << endl;
+	cout << " 홍\t100\t100\t100" << endl;
+	cout << " 길\t 98\t 45\t 65" << endl;
+	cout << " 동\t 10\t 20\t  0" << endl;
+}
+
+
+// 들여쓰기 tab
+// 가독성이 좋게
+// ; 마침표다
+// endl 화면에서 한줄 밑으로 내려라
+// cout 화면에 문자를 출력
+// 주석 없는 코드나 마찬가지. 코드의 부가적인 설명을 할 때 사용. /*문장*/ , //한줄
+// main 함수는 프로그램이 시작되는 startup 함수다.
+
+/*
+int main()
+{
+	return 0; <==== 프로그램이 끝나는 지점.
+	// int가 붙으면 return 해준다.
+}
+*/
